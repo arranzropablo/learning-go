206e61476252e3e69f6643c1daf7f06f3251d4a4
Pablo <Pablo.ArranzRopero@zooplus.com>
2019-01-18 13:32:19 +0100

    started learning golang

---
+++ intro/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	//We cant declare variables or import packages which wont be used, because it wont compile
+	"fmt"
+)
+
+func main() {
+	//fmt is the standard i/o system
+	fmt.Println("Hello world")
+}
