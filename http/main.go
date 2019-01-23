07df61cede86d0736ddd63492cf23a0c145295da
Pablo <Pablo.ArranzRopero@zooplus.com>
2019-01-23 14:36:02 +0100

    finished http

--- http/main.go
+++ http/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 )
+
+type logWriter struct{}
 
 func main() {
 	//In Go only vars and funcs starting with capital letter are exported, those with lower letter are "private"
@@ -11,19 +14,23 @@ func main() {
 	response, err := http.Get("http://www.google.com")
 	if err == nil {
 
-		//We initialize it with this number bc the Read function dont resize the [], just copy the body into []byte and if it is full it will fail
+		//
+/*		//We initialize it with this number bc the Read function dont resize the [], just copy the body into []byte and if it is full it will fail
 		bs := make([]byte, 99999)
 		//We pass bs to Read which fills it (return num of bytes and error, not a byte slice)
 		response.Body.Read(bs)
-		fmt.Println(string(bs))
+		fmt.Println(string(bs))*/
 
-		//We don't read like that, Go has helpers, one way found on internet is:
-		//buf := new(bytes.Buffer)
-		//buf.ReadFrom(response.Body)
-		//newStr := buf.String()
-		//fmt.Println(newStr)
+		//instead of all that Read stuff we can directly copy the body to stdout (io.Copy uses a Writer and a Reader)
+		//io.Copy(os.Stdout, response.Body)
+
+		//We can use our own type if it implements the interface Writer
+		io.Copy(logWriter{}, response.Body)
+
 	}
+}
 
-
-
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	return len(bs), nil
 }
