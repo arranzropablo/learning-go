1c0ef2f793ffd13bf2f749a0d078da4f6b8c3701
Pablo <Pablo.ArranzRopero@zooplus.com>
2019-01-24 17:11:45 +0100

    goroutines

--- website-status-checker/main.go
+++ website-status-checker/main.go
@@ -15,11 +15,32 @@ func main() {
 		"http://amazon.com",
 	}
 
+	//this takes too long because http.Get is a blocking call
+	//for _, link := range links {
+	//	if _, err := http.Get(link); err != nil{
+	//		fmt.Println("Error during request to", link)
+	//	} else {
+	//		fmt.Println(link, "is up!")
+	//	}
+	//}
+
 	for _, link := range links {
-		if _, err := http.Get(link); err != nil{
-			fmt.Println("Error during request to", link)
-		} else {
-			fmt.Println(link, "is up!")
-		}
+		//with go instruction, it executes a call in a different goroutine
+		//the go scheduler runs one routine until it finishes or makes a blocking call
+		//By default Go tries to use one core but each CPU can run one go routine
+		//By default we have the main routine and if we use the keyword "go" we create child routines
+		go checkLink(link)
+	}
+}
+
+//Concurrency vs Parallelism
+//Concurrency: If one thread blocks another thread is picked up (but they don't execute simultaneously)
+//Parallelism: We can execute multiple threads at the same time (multiple CPUs required)
+
+func checkLink(link string) {
+	if _, err := http.Get(link); err != nil{
+		fmt.Println("Error during request to", link)
+	} else {
+		fmt.Println(link, "is up!")
 	}
 }
